31c053274a035213c2eff2afc213a3eeca893bfd
Leander-Botha <578404@student.belgiumcampus.ac.za>
2022-08-28 21:37:03 +0200

    Delete, close , Detail button functionality

--- ticket.js
+++ ticket.js
@@ -53,6 +53,7 @@ function submitTicket() {
      let AssignedEmp = document.getElementById("assignedEmployees").value;
      let ticketdescription = document.getElementById("ticketdescription").value;
      let ticketPriority = document.getElementById("ticketPriority").value;
+     let targdate = document.getElementById("targdate").value;
      let ticketSubmitDate =  new Date().toDateString;
      let bugFounder = document.getElementById("bugFounder").value;
      //let actdate = document.getElementById("actualdate").value;
@@ -67,9 +68,10 @@ function submitTicket() {
        description: ticketdescription,
        assignedTo: AssignedEmp,
        priority: ticketPriority,
-       SubDate: new Date().toDateString(),
+       SubDate: ticketSubmitDate,
        Founder: bugFounder,
-       actualdate: "",
+       actualdate: "Pending",
+       targdate:targdate, //Display in ticket summary also.
        resolveSummary : "",
        ticketSum: ticketSum,
        status: "Open"
@@ -91,111 +93,64 @@ function submitTicket() {
  
 
 
- function fetchTickets() {
+ function ShowAllTickets() {
     let tickets = JSON.parse(localStorage.getItem('Tickets'));
     
     let ticketList = document.getElementById('div-container');
     
-    ticketList.innerHTML ='';
+
     
     for (let i = 0; i < tickets.length; i++) {
       let id = tickets[i].id;
       let name = tickets[i].name;
-      let desc = tickets[i].description;
+      let summ = tickets[i].ticketSum;
       let priority = tickets[i].priority;
       let assignedTo = tickets[i].assignedTo;
       let status = tickets[i].status;
-      let subDate = tickets[i].subDate;
+      let subDate = tickets[i].SubDate;
+      let actualdate = tickets[i].actualdate;
       
-      ticketList.innerHTML += '<div class = "well">' +
-                          '<h6>Issue name: ' + name + '</h6>' + 
-                          '<p><span class = "label label-info">' + status + '</span></p>' +
-                          '<h3>' + desc + '</h3>' +
-                          '<p><span></span> ' + priority + '</p>' +
-                          '<p><span></span> ' + assignedTo + '</p>' + 
-                          '<p><span></span> ' + subDate + '</p>' + 
-                          '<a href = "#" onclick = "CloseTicket(\''+id+'\')" class = "btn btn-warning">Close</a> ' +
-                          '<a href = "#" onclick = "deleteTicket(\''+id+'\')" class = "btn btn-danger">Delete</a>' +
+      ticketList.innerHTML += '<div class = "child-div">' +
+                          "<h6>Ticket ID: " + id + "</h6>" +
+                          "<h1>Ticket name: " + name + "</h1>" + 
+                          "<p> Status: " + status + "</p>" +
+                          "<p> Summary: " + summ + "</h3>" +
+                          "<p> Priority: " + priority + "</p>" +
+                          "<p> Assigned to: " + assignedTo + "</p>" + 
+                          "<p> Submission Date: " + subDate + "</p>" + 
+                          "<p> Resolved on: " + actualdate + "</p>" +
+                          '<a href="#Detail-top" onclick = "ViewMoreDetails(\''+id+'\')" class = "btn btn-info">Details'+'</a>' + "     "+
+                          '<button onclick = "EditTicket(\''+id+'\')" class = "btn btn-dark">Edit'+"</button>"  + "     " +
+                          '<button onclick = "CloseTicket(\''+id+'\')" class = "btn btn-primary">Close' +"</button>" + "    " +
+                          '<button onclick = "DeleteTicket(\''+id+'\')" class = "btn btn-danger">Delete'+"</button>" + "     " + 
+
                            '</div>';
     }
-   // document.getElementById("div-container").style.display="block-inline"; 
   }
 
+function viewMoreDetails() 
+{
+ console.log("More details were shown");
+}
+
+
+function DeleteTicket(ticketID)
+{
+    let ticketArray = JSON.parse(localStorage.getItem('Tickets'));
+    for(let i=0; i<ticketArray.length; i++) 
+    {
+        if(ticketArray[i].id == ticketID)
+        {
+            ticketArray.splice(i,1);
+        }
+    }
+    localStorage.setItem('Tickets', JSON.stringify(ticketArray)); 
+    window.location.reload(true);
+    console.log("Deleted: " + ticketID)
+}
 
 // Display ticket on click
-/*function DisplayTicket(ticketArray)
-{
-    const DivContainer = document.getElementById("div-container");
-    const newTkt = document.createElement("div");
-    // Add projectID/Name to display.
-    const newTktName = document.createElement("h1");
-    const newTktSumm = document.createElement("p");
-    const newTktPrio = document.createElement("select");
-    const newtTktAssign = document.createElement("select");
-    const newTktType = document.createElement("select");
-    const newTktDate = document.createElement("p");
-    
-    const newTktMoreDetailsBtn = document.createElement("button");
 
-     console.log("add");
-
-     newTkt.classList.add("child-div");
-
-
-     DivContainer.appendChild(newTkt);
-
-     newTkt.appendChild(newTktName);
-     newTkt.appendChild(newTktSumm);
-     newTkt.appendChild(newTktPrio);
-     newTkt.appendChild(newtTktAssign);
-     newTkt.appendChild(newTktType);
-     newTkt.appendChild(newTktDate);
-     newTkt.appendChild(newTktMoreDetailsBtn);
-
-     //newTkt.addEventListener("click",viewTicket);
-   
-
-     newTktName.classList.add("tktName");
-     newTktSumm.classList.add("tktSummary");
-     newTktPrio.classList.add("tktPriority");
-     newTktType.classList.add("tktType");
-     newtTktAssign.classList.add("tktAssign")
-     newTktDate.classList.add("tktDate");
-
-     //newTktMoreDetailsBtn.setAttribute("id","tktMoreDetailsBtn");
-     newTktMoreDetailsBtn.id = "tktMoreDetailsBtn";
-     // Add open and close buttons.
-     newTktMoreDetailsBtn.innerHTML = "View more details";
-
-
-     newTkt.id = click;
-     click++;
-
-     for(let i = 0; i < ticketArray.length; i++)
-     {   
-        if(newTkt.id == ticketArray[i].id)
-        {
-            newTktName.innerHTML = ticketArray[i].name;
-            newTktSumm.innerHTML = ticketArray[i].summ;
-            newTktPrio.innerHTML = ticketArray[i].prio;
-            newtTktAssign.innerHTML = ticketArray[i].assignedTo;
-            newTktType.innerHTML = ticketArray[i].type;
-            newTktDate.innerHTML = ticketArray[i].date;
-        
-        } 
-        /*else if(click > tickets.length-1)
-        {
-             BtnAdd.disabled = true; 
-        }*/
-
-
-     /*}
-
-     newTktMoreDetailsBtn.addEventListener("click", DisplayTicketDetails)
-
-     console.log("Printed from display function: ");
-     console.log(ticketArray);
-}*/
 // Show all tickets
 /*function ShowAllTickets(){
 
@@ -262,46 +217,46 @@ function submitTicket() {
     
 }*/
 
-function DisplayTicketDetails()
+function ViewMoreDetails()
 {
+    let ticketArray = JSON.parse(localStorage.getItem('Tickets'));
+
     document.getElementById("ticketDetails").style.display="block";
     document.getElementById("addNewTicket").style.display="none"; 
-    const addNewTicketBtn = document.createElement("addNewTicketBtn");
 
     //document.getElementById("div-container").style.display="none";
 
-   const DetailTktName = document.getElementById("ticketTitle");
-   const DetailProjName = document.getElementById("projectName");
-   const DetailIdentifiedby = document.getElementById("identifiedBy");
-   const DetailAssign = document.getElementById("lstassignedEmp");
-   const DetailPrio = document.getElementById("detpriority");
-   const DetailType = document.getElementById("dettype");
-   const DetailDesc = document.getElementById("ticketDescription");
-   const DetailDateIdent = document.getElementById("dateIdenfiied");
-   const DetailTargDate = document.getElementById("trgtResolutionDate");
-   const DetailActDate = document.getElementById("actResolutionDate");
-   const DetailResolSum = document.getElementById("resolSummary");
 
-   const listAssingedEmp = document.getElementById("lstassignedEmp");
 
-   addEventListener('click', function(event)
-   {
-    if(event.target.id !== '')
-    {
+
         for(let i = 0; i < ticketArray.length; i++)
         {   
-           if(event.target.parentElement.id == ticketArray[i].id)
-           {
+
+        const DetailTktID = document.getElementById("ticketID")
+        const DetailTktName = document.getElementById("ticketTitle");
+        const DetailProjName = document.getElementById("projectName");
+        const DetailIdentifiedby = document.getElementById("identifiedBy");
+        const DetailAssign = document.getElementById("assignedEmp");
+        const DetailPrio = document.getElementById("detpriority");
+        const DetailType = document.getElementById("dettype");
+        const DetailDesc = document.getElementById("ticketDescription");
+        const DetailDateIdent = document.getElementById("dateIdenfiied");
+        const DetailTargDate = document.getElementById("trgtResolutionDate");
+        const DetailActDate = document.getElementById("actResolutionDate");
+        const DetailResolSum = document.getElementById("resolSummary");
+
+        const listAssingedEmp = document.getElementById("lstassignedEmp");
+               DetailTktID.innerHTML = ticketArray[i].id
                DetailTktName.innerHTML = ticketArray[i].name;
-               DetailProjName.innerHTML = ticketArray[i].projectID;
-               DetailIdentifiedby.innerHTML = ticketArray[i].assignedTo;
+               DetailProjName.innerHTML = ticketArray[i].projectName;
+               DetailIdentifiedby.innerHTML = ticketArray[i].Founder;
                DetailAssign.innerHTML = ticketArray[i].assignedTo;
                DetailPrio.innerHTML = ticketArray[i].priority;
-               DetailType.innerHTML = ticketArray[i].type;
-               DetailDesc.innerHTML = ticketArray[i].desc;
-               DetailDateIdent.innerHTML = ticketArray[i].date;
+               DetailType.innerHTML = ticketArray[i].status;
+               DetailDesc.innerHTML = ticketArray[i].description;
+               DetailDateIdent.innerHTML = ticketArray[i].subDate;
                DetailTargDate.innerHTML = ticketArray[i].targdate;
-               DetailActDate.innerHTML = ticketArray[i].actdate;
+               DetailActDate.innerHTML = ticketArray[i].actualdate;
                DetailResolSum.innerHTML = ticketArray[i].resolsum;
 
                for(const User of users)
@@ -311,45 +266,23 @@ function DisplayTicketDetails()
                    option.innerHTML = User.name;
                    listAssingedEmp.appendChild(option);
                }
-           } 
-        }
-        
-        
-        console.log(event.target.parentElement.id);
-        return event.target.parentElement.id;
-    }
-    else
-    {
-         console.log("No element id ");
-    }
-
-   });
-
+        }     
 } 
 
 
-function deleteTicket(id)
+function CloseTicket(ticketID)
 {
-    let Tickets =JSON.parse(localStorage.getItem('Tickets'));
-    for (let i = 0; i < Tickets.length; i++) 
+    let ticketArray = JSON.parse(localStorage.getItem('Tickets'));
+    for (let i = 0; i < ticketArray.length; i++) 
     {
-        if (Tickets[i].id == id)
+        if (ticketArray[i].id == ticketID)
         {
-            Tickets.splice(i, 1);          
+            ticketArray[i].status ="Resolved";
+            ticketArray[i].actualdate = new Date().toDateString();
         }
     }
-}
-
-function CloseTicket(id)
-{
-    let Tickets =JSON.parse(localStorage.getItem('Tickets'));
-    for (let i = 0; i < Tickets.length; i++) 
-    {
-        if (Tickets[i].id == id)
-        {
-            Tickets.status ="status";
-        }
-    }
+    console.log("Closed");
+    localStorage.setItem('Tickets', JSON.stringify(ticketArray));
 }
 
 
